4a322896f7c2e65dfe089c9d8cca5cafc2cf569e
KevinCaleb <39257362+KevinCaleb@users.noreply.github.com>
2018-05-14 13:15:59 +0530

    new folder added

---
+++ app/src/main/java/francis/grmcinc/com/androidbrowser/MainActivity.java
@@ -0,0 +1,21 @@
+package francis.grmcinc.com.androidbrowser;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+}
